1c300ec4093f38f1413340522284d0f369386c98
Serguei Nastassi <115593821+itoracl@users.noreply.github.com>
2023-01-29 17:42:48 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './2CA25D51-B177-4C32-A51D-EC193CB3CE32_adobe_express.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          JP Moscow Bank
+        </p><p></p><p></p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p></p><p></p><p></p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          [     Я клиент     ]
+        </a>
+        <p></p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          [Зарегистрироваться]
+        </a>
+    </header>
+    </div>
+  );
+}
+
+export default App;
